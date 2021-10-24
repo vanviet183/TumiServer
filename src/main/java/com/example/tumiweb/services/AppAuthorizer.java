1b8b98ae151277bb9e052a4c6fd501b875e0d83c
Only Huân <71635685+huannd0101@users.noreply.github.com>
2021-10-24 09:25:57 +0700

    update

--- src/main/java/com/example/tumiweb/services/AppAuthorizer.java
+++
@@ -1,7 +0,0 @@
-package com.example.tumiweb.services;
-
-import org.springframework.security.core.Authentication;
-
-public interface AppAuthorizer {
-    boolean authorize(Authentication authentication, String role, Object callerObj);
-}
